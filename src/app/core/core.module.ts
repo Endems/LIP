9fac7324d8d00446ba58a0a69c756c273a61ef21
LucaoMendes <lucas1999mendes@gmail.com>
2025-05-04 19:29:51 -0300

    LIP-2 - Criação do compoente HERO

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import { HeroComponent } from './home/sections/hero/hero.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    HeroComponent
   ],
   imports: [
     // BrowserModule,
